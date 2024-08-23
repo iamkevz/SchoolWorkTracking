2eb7137cd7cf108d5d5a042005cc647be389fce1
kev.z <kevinreyalcantara@gmail.com>
2024-08-23 13:57:51 +0800

    add tags function

---
+++ src/components/AssignmentTags.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="flex gap-3">
+    <button
+      @click="$emit('update:modelValue', tag)"
+      v-for="tag in tags"
+      class="border rounded px-1 py-px text-xs"
+      :class="{
+        'border-blue-500 text-blue-500': tag === modelValue,
+      }"
+    >
+      {{ tag }}
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    initialTags: Array,
+    modelValue: String
+  },
+  
+  computed: {
+    tags() {
+      return ['all', ...new Set(this.initialTags)];
+    },
+  },
+};
+</script>
