2e5e79c4357d9ec1503c38982f8259e38bad2a0c
kev.z <kevinreyalcantara@gmail.com>
2024-08-23 17:52:03 +0800

    implement creation of assignment and panel

--- src/components/AssignmentList.vue
+++ src/components/AssignmentList.vue
@@ -1,9 +1,14 @@
 <template>
-  <section v-show="assignments.length">
-    <h2 class="font-bold mb-2">
+  <Panel v-show="assignments.length" class="w-68 ">
+    <div class="flex justify-between items-start">
+        <h2 class="font-bold mb-2">
       {{ title }}
       <span>({{ assignments.length }})</span>
     </h2>
+
+    <button v-show="canToggle" @click="$emit('toggle')">&times;</button>
+    </div>
+    
 
     <assignment-tags
       v-model="currentTag"
@@ -17,21 +22,32 @@
         :assignment="assignment"
       ></assignment>
     </ul>
-  </section>
+
+    <slot></slot>
+
+    <template #footer>
+        my footer goes here
+    </template>
+  </Panel>
 </template>
 
 <script>
 import AssignmentTags from "./AssignmentTags.vue";
 import Assignment from "./Assignment.vue";
+import AssignmentCreate from "./AssignmentCreate.vue";
+import Panel from "./Panel.vue";
 
 export default {
   components: {
     Assignment,
     AssignmentTags,
+    AssignmentCreate,
+    Panel
   },
   props: {
     assignments: Array,
     title: String,
+    canToggle: {type: Boolean, default:false}
   },
   data() {
     return {
--- src/components/Assignments.vue
+++ src/components/Assignments.vue
@@ -1,16 +1,17 @@
 <template>
-  <section class="space-y-6">
-    <assignment-list
-      :assignments="filters.inProgress"
-      title="In Progress"
-    ></assignment-list>
+  <section class="flex gap-8">
+    <assignment-list :assignments="filters.inProgress" title="In Progress">
+      <assignment-create @add="add" />
+    </assignment-list>
 
-    <assignment-list
-      :assignments="filters.completed"
-      title="Completed"
-    ></assignment-list>
-
-    <assignment-create @add="add"></assignment-create>
+    <div v-show="showCompleted">
+      <assignment-list
+        :assignments="filters.completed"
+        title="Completed"
+        can-toggle
+        @toggle="showCompleted = !showCompleted"
+      ></assignment-list>
+    </div>
   </section>
 </template>
 
@@ -23,10 +24,11 @@ export default {
   data() {
     return {
       assignments: [
-        { name: "Finish project", complete: false, id: 1, tag: 'english' },
-        { name: "Read Chapter 4", complete: false, id: 2, tag: 'math' },
-        { name: "Turn in Homework", complete: false, id: 3, tag: 'math'},
+        /*  { name: "Finish project", complete: false, id: 1, tag: "english" },
+        { name: "Read Chapter 4", complete: false, id: 2, tag: "math" },
+        { name: "Turn in Homework", complete: false, id: 3, tag: "math" }, */
       ],
+      showCompleted: true,
     };
   },
   computed: {
@@ -38,6 +40,13 @@ export default {
         completed: this.assignments.filter((assignment) => assignment.complete),
       };
     },
+  },
+  created() {
+    fetch("http://localhost:8000/assignments")
+      .then((response) => response.json())
+      .then((assignments) => {
+        this.assignments = assignments;
+      });
   },
   methods: {
     add(name) {
