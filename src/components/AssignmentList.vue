d8eafec04f95f92c571664cecd61648481a85418
kev.z <kevinreyalcantara@gmail.com>
2024-08-23 12:30:32 +0800

    create assignmentlist

---
+++ src/components/AssignmentList.vue
@@ -0,0 +1,25 @@
+<template>
+  <section v-show="assignments.length">
+    <h2 class="font-bold mb-2">{{ title }}</h2>
+    <ul class="border border-gray-680 divide-y divide-gray-680">
+      <assignment
+        v-for="assignment in assignments"
+        :key="assignment.id"
+        :assignment="assignment"
+      ></assignment>
+    </ul>
+  </section>
+</template>
+
+<script>
+import Assignment from "./Assignment.vue";
+export default {
+  components: {
+    Assignment,
+  },
+  props: {
+    assignments: Array,
+    title: String,
+  },
+};
+</script>
