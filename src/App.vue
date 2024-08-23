76825f80a8d5d308643e19ce3594bedb82b016cf
kev.z <kevinreyalcantara@gmail.com>
2024-08-23 17:50:43 +0800

    add implementation of panel component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <div class="bg-gray-800 text-white">
+  <div class="grid gap-6 ">
     <Assignments></Assignments>
+
   </div>
 </template>
 
 <script>
 import Assignments from "./components/Assignments.vue";
-
+import Panel from "./components/Panel.vue";
 export default {
   components: {
     Assignments,
+    Panel,
   },
 };
 </script>
